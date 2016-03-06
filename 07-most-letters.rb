42ff00c4fc4c44062ae4992d1787899c801ecb84
gabriel_lewis <gabrielhlewis@gmail.com>
2016-03-06 04:29:56 +0000

    Finished problem 7

--- 07-most-letters.rb
+++ 07-most-letters.rb
@@ -5,14 +5,42 @@
 # Difficulty: medium.
 
 def nearby_az(string)
+    
+    a_present = false
+    z_present = false
+    i = 0 
+    a_location = 0
+    letter = nil
+    
+    while i < string.length
+     letter = string[i]
+        if a_present
+            if i <= a_location
+             if letter == "z"
+                 z_present = true
+             end
+            end
+        elsif letter == "a"
+         a_present = true
+            a_location = i + 3
+        end
+      i = i + 1
+    end
+    if z_present == true
+        return true
+    else
+        return false
+    end
+    
 end
 
 # These are tests to check that your code is working. After writing
 # your solution, they should all print true.
-
 puts('nearby_az("baz") == true: ' + (nearby_az('baz') == true).to_s)
 puts('nearby_az("abz") == true: ' + (nearby_az('abz') == true).to_s)
 puts('nearby_az("abcz") == true: ' + (nearby_az('abcz') == true).to_s)
 puts('nearby_az("a") == false: ' + (nearby_az('a') == false).to_s)
 puts('nearby_az("z") == false: ' + (nearby_az('z') == false).to_s)
 puts('nearby_az("za") == false: ' + (nearby_az('za') == false).to_s)
+
+
