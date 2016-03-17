f43cc6879995ada5b8ada2aa9b905abdbc5791d0
gabriel_lewis <gabrielhlewis@gmail.com>
2016-03-17 20:56:58 +0000

    Finished problem 13

--- 13-capitalize-words.rb
+++ 13-capitalize-words.rb
@@ -9,6 +9,30 @@
 # Difficulty: medium.
 
 def capitalize_words(string)
+  words_array = string.split(" ")
+  
+  i = 0
+  while i < words_array.length
+      word = words_array[i].split("")
+      
+      idx = 0 
+      
+      while idx <= word.length
+        if idx == 0 
+          word[idx] = word[idx].to_s.upcase
+          
+        end
+        
+       idx += 1
+       
+      end
+      words_array[i] = word.join
+      #print(word.join)
+    i += 1
+  end
+  #print(words_array)
+  return(words_array.join(" "))
+  
 end
 
 # These are tests to check that your code is working. After writing
