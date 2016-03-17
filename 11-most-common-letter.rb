aff6ecce5bdee738002a5af39e705ca8706582d7
gabriel_lewis <gabrielhlewis@gmail.com>
2016-03-17 19:50:28 +0000

    finished Problem 11

--- 11-most-common-letter.rb
+++ 11-most-common-letter.rb
@@ -5,6 +5,36 @@
 # Difficulty: medium.
 
 def most_common_letter(string)
+  common_counter = nil
+  common = nil
+  
+  
+  
+
+ 
+  
+  i = 0 
+  
+  while i < string.length
+    counter = 0
+    letter = string[i]
+    idx2 = 0
+    
+    while idx2 < string.length
+      letter2 = string[idx2]
+      if letter == letter2
+        counter = counter + 1
+      end
+      idx2 = idx2 + 1
+    end
+    if (common == nil) || (counter > common_counter)
+      common = letter
+      common_counter = counter
+    end
+    i = i + 1
+  end
+  
+  return [common, common_counter]
 end
 
 # These are tests to check that your code is working. After writing
