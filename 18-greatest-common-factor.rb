d62d88d23e01e02bc8678cb29717f87b39f4ad68
gabriel_lewis <gabrielhlewis@gmail.com>
2016-03-19 22:23:56 +0000

    finished problem 18

--- 18-greatest-common-factor.rb
+++ 18-greatest-common-factor.rb
@@ -5,6 +5,33 @@
 # Difficulty: medium.
 
 def greatest_common_factor(number1, number2)
+  i = 1 
+  number1_factor = nil
+  number2_factor = nil
+  gcd = nil
+  
+  while i <= number1
+    if number1 % i == 0 
+      number1_factor = i
+    end
+    i2 = 1
+    while i2 <= number2
+      if number2 % i2 == 0
+        number2_factor = i2
+        if number2_factor == number1_factor
+         if gcd == nil
+           gcd = number1_factor
+         elsif gcd < number1_factor
+           gcd = number1_factor
+         end
+             
+        end
+      end
+      i2 += 1
+    end
+    i += 1 
+  end
+  return(gcd)
 end
 
 # These are tests to check that your code is working. After writing
